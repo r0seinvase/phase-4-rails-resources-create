ba2b50b9fdcdd8df5c36be31742f41bae276234f
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-04-27 14:13:23 -0500

    commit tree 7cb4ef54824eac1363c80c9ef7f4dce704ec47a1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+def create 
+    bird = Bird.create(name: params[:name], species: params[:species])
+  render json: bird, status: :created 
+end
+
+def index
+    birds = Bird.all
+end
+
 end
